7ee55ebab6ecaf7cc2d5a00240294f8df2f5247c
Dharmik_403 <hingudharmik4@gmail.com>
2022-07-26 20:14:18 +0530

    feat: adds more colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,12 @@ module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
 		extend: {},
+		colors: {
+			secondary: '#dae0e6',
+			primary: '#f8f9fa',
+			darkText: '#222222',
+			lightText: '#787c7e',
+		},
 	},
 	plugins: [],
 };
