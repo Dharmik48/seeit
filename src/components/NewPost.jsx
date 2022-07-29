e8c1520ac43cf111458f06e698964238e45feaa5
Dharmik_403 <hingudharmik4@gmail.com>
2022-07-29 19:33:04 +0530

    feat: adds basic google sign in

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,46 +1,30 @@
 import { useState } from 'react';
 
-// Import React FilePond
-import { FilePond, registerPlugin } from 'react-filepond';
-// Import FilePond styles
-import 'filepond/dist/filepond.min.css';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { storage } from '../firebase/firebase';
+import { v4 } from 'uuid';
 
-// Import the Image EXIF Orientation and Image Preview plugins
-// Note: These need to be installed separately
-// `npm i filepond-plugin-image-preview filepond-plugin-image-exif-orientation --save`
-import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
-import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
-import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
-
-// Register the plugins
-registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
-
-// Our app
 export default function NewPost() {
-	// const [files, setFiles] = useState([]);
+	const [imgUrl, setImgUrl] = useState('');
 
-	function setImg(e) {
-		// const img = new Image();
+	function uploadImg(e) {
+		if (!e.target.files[0]) return;
 
-		if (e.target.files.length > 0) {
-			console.log(e.target.files[0]);
-			console.log(URL.createObjectURL(e.target.files[0]));
-		}
-		// console.log(e.target.files[0]);
+		const image = e.target.files[0];
+		const imgRef = ref(storage, `/images/${v4()}`);
+		uploadBytes(imgRef, image).then(async () => {
+			const url = await getDownloadURL(imgRef);
+			setImgUrl(url);
+		});
 	}
 
 	return (
-		<div className='App'>
-			{/* <FilePond
-				files={files}
-				onupdatefiles={setFiles}
-				onpreparefile={setImg()}
-				maxFiles={1}
-				name='files'
-				server='/'
-				labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
-			/> */}
-			<input type='file' onChange={e => setImg(e)} />
+		<div className='bg-primary border border-[#ccc] rounded-lg shadow-lg'>
+			<form>
+				<input type='text' placeholder='New Post' />
+				<input type='file' onChange={e => uploadImg(e)} />
+			</form>
+			{imgUrl && <img src={imgUrl} />}
 		</div>
 	);
 }
