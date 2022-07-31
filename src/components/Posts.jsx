3685dba5f4d2c6f9b933cdca2e51be9f999078c7
theabhayprajapati <88815641+theabhayprajapati@users.noreply.github.com>
2022-07-31 20:20:40 +0530

    added dark mode

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -4,7 +4,7 @@ export default function Posts({ posts }) {
 	const renderPosts = () => posts.map(post => <Post data={post} />);
 
 	return (
-		<section className='grid gap-5 font-light mt-5 lg:mt-10'>
+		<section className='grid gap-5 font-light mt-5 lg:mt-10 dark:bg-darkText'>
 			{renderPosts()}
 		</section>
 	);
---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const useDarkMode = () => {
+    const [theme, setTheme] = useState("light");
+    const colorTheme = theme === "light" ? "dark" : "light";
+    useEffect(() => {
+        const root = window.document.documentElement;
+        root.classList.add(theme);
+        root.classList.remove(colorTheme);
+
+    }
+        , [setTheme, colorTheme]);
+        return [setTheme, colorTheme];
+}
+
+
+export default useDarkMode
