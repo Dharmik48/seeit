755f05d2214b79b6eb2ddc75b9845ad6f15a3045
Dharmik_403 <hingudharmik4@gmail.com>
2022-08-03 21:44:39 +0530

    feat: adds new success flash

--- src/components/FlashMsg.jsx
+++ src/components/FlashMsg.jsx
@@ -1,10 +1,14 @@
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function FlashMsg({ msg, setFlash }) {
+export default function FlashMsg({ flash, setFlash }) {
+	const bgColor = flash.success ? 'bg-brightGreen' : 'bg-brightRed';
+
 	return (
-		<div className='max-w-fit flex items-center gap-10 justify-between text-primary bg-brightRed sticky py-3 px-5 rounded-lg shadow-lg top-5 mx-auto'>
-			{msg}
+		<div
+			className={`max-w-fit flex items-center gap-10 justify-between text-primary  sticky py-3 px-5 rounded-lg shadow-lg top-5 mx-auto ${bgColor}`}
+		>
+			{flash.msg}
 			<FontAwesomeIcon
 				icon={faTimes}
 				className='cursor-pointer'
