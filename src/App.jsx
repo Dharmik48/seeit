0a097b1df73573702edf72733739766ce0326c70
mdazad1508 <mdazad15081999@gmail.com>
2022-08-05 23:16:58 +0530

    sperate header file has been created and added in components

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Posts from './components/Posts';
 import { auth, colRef, provider } from './firebase/firebase';
 import useDarkMode from './hooks/useDarkMode';
 import FlashMsg from './components/FlashMsg';
+import Header  from './components/Header';
 
 function App() {
 	const [posts, setPosts] = useState([]);
@@ -63,44 +64,7 @@ function App() {
 
 	return (
 		<main className='w-full min-h-screen py-5 bg-secondary px-5 font-primary dark:bg-darkText'>
-			<header
-				className={`max-w-3xl flex items-center justify-between mx-auto border-b border-darkText pb-3 lg:pb-5 ${
-					user && 'mb-5 lg:mb-10'
-				}`}
-			>
-				<h1 className='text-2xl dark:text-primary'>👀 SeeIt</h1>
-				<div className='flex items-center'>
-					<label
-						className='flex items-center'
-						onClick={() => setTheme(colorTheme)}
-					>
-						<input type='checkbox' className='' />
-					</label>
-				</div>
-				{!user ? (
-					<button
-						onClick={() => signInWithGoogle()}
-						className='border border-darkText rounded-full py-1.5 px-4 hover:bg-darkText hover:text-primary transition-colors focus:bg-darkText focus:text-primary focus:outline-none dark:text-primary dark:bg-darkText
-            dark:hover:bg-darkText dark:hover:text-primary dark:border-primary'
-					>
-						Sign In
-					</button>
-				) : (
-					<div className='flex items-center gap-3.5 lg:gap-5'>
-						<span
-							className='cursor-pointer dark:text-primary'
-							onClick={signUserOut}
-						>
-							Sign out
-						</span>
-						<img
-							src={user.photoURL}
-							className='max-h-8 lg:max-h-10 rounded-full'
-							alt={user.displayName}
-						/>
-					</div>
-				)}
-			</header>
+            <Header setTheme={setTheme} colorTheme={colorTheme} user={user} signInWithGoogle={signInWithGoogle} signUserOut={signUserOut} />
 			<section className='max-w-xl mx-auto grid relative'>
 				{flash.show && <FlashMsg flash={flash} setFlash={setFlash} />}
 				{user && <NewPost />}
