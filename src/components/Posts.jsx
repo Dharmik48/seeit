2aa207a79b4914be8f6359223cbb176d306b87f4
Dharmik_403 <hingudharmik4@gmail.com>
2022-08-12 19:46:05 +0530

    fix: removes inefficient logic

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,24 +1,17 @@
 import {useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
-import {doc, getDoc, onSnapshot, orderBy, query} from "firebase/firestore";
-import {db, postsColRef} from "../firebase/firebase.js";
+import {onSnapshot, orderBy, query} from "firebase/firestore";
+import {postsColRef} from "../firebase/firebase.js";
 import Post from './Post';
 
 export default function Posts({ currentUser, setFlash }) {
 	const [posts, setPosts] = useState([]);
-	const {postId} = useParams();
 
 	useEffect(() => {
-		if (!postId) {
-			onSnapshot(query(postsColRef, orderBy('createdAt', 'desc')), snapshot => {
-				const data = snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
-				setPosts(data);
-			});
-		} else {
-			getDoc(doc(db, 'posts', postId))
-				.then(data => setPosts([{...data.data(), id:postId}]))
-		}
-	}, [postId]);
+		onSnapshot(query(postsColRef, orderBy('createdAt', 'desc')), snapshot => {
+			const data = snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+			setPosts(data);
+		});
+	}, []);
 
 	const renderPosts = () =>
 		posts.map(post => (
