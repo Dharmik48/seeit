5c02083e8e4c2f12926cea33674e3f231a83b072
Dharmik_403 <hingudharmik4@gmail.com>
2022-08-20 20:53:38 +0530

    chore: removes todo comment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ import Auth from "./Auth";
 const Header = () => {
   return (
     <header className="max-w-3xl flex justify-between mx-auto border-b border-darkText pb-3 lg:pb-5 mb-5 lg:mb-10">
-      {/* TODO: add a link to home to the logo */}
       <Link to={"/"}>
         <h1 className="text-2xl dark:text-primary">👀 SeeIt</h1>
       </Link>
