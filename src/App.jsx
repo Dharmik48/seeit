c74d4ebdd7e14db810ed4083887fc049606de42d
Anmol Pokhrel <anmolpokhrel46@gmail.com>
2022-08-24 08:39:49 +0530

    persist user login untill explicit logout

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,22 @@ import FlashMsg from "./components/FlashMsg";
 import Header from "./components/Header";
 import ThemeToggle from "./components/ThemeToggle.jsx";
 import PostDetail from "./components/posts/PostDetail.jsx";
+import { useEffect } from "react";
+import { auth } from "./firebase/firebase.js";
 
 function App() {
-    const { user } = useContext(UserContext);
+    const { user, setUser } = useContext(UserContext);
     const { flashData } = useContext(FlashContext);
-
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                const { uid, photoURL, email, displayName } = user;
+                setUser({ uid, photoURL, email, displayName });
+            } else {
+                console.log("not logged in");
+            }
+        });
+    }, []);
     return (
         <main className="w-full min-h-screen py-5 bg-secondary px-5 font-primary dark:bg-darkText">
             {flashData?.show && <FlashMsg />}
@@ -22,15 +33,8 @@ function App() {
                 <ThemeToggle />
                 {user && user.email && <NewPost />}
                 <Routes>
-                    <Route
-                        exact
-                        path="/"
-                        element={<Posts />}
-                    />
-                    <Route
-                        path="/posts/:postId"
-                        element={<PostDetail />}
-                    />
+                    <Route exact path="/" element={<Posts />} />
+                    <Route path="/posts/:postId" element={<PostDetail />} />
                 </Routes>
             </section>
         </main>
