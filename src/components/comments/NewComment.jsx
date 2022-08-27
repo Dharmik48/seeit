29b2cd2cae4576c92fad1fcf7da3aafb664c6393
Dharmik_403 <hingudharmik4@gmail.com>
2022-08-27 19:32:19 +0530

    chore: updates placeholder

--- src/components/comments/NewComment.jsx
+++ src/components/comments/NewComment.jsx
@@ -1,24 +1,25 @@
-import {useContext, useState} from "react";
+import { useContext, useState } from "react";
 import UserContext from "../../contexts/UserContext";
 import FlashContext from "../../contexts/FlashContext.jsx";
 import {
   addDoc,
   serverTimestamp,
   updateDoc,
-  increment, doc,
+  increment,
+  doc,
 } from "firebase/firestore";
-import {db} from "../../firebase/firebase.js";
+import { db } from "../../firebase/firebase.js";
 
-export default function NewComment({commentsColRef, postId}) {
+export default function NewComment({ commentsColRef, postId }) {
   const [commentText, setCommentText] = useState("");
-  const {user: currentUser} = useContext(UserContext);
-  const {flash} = useContext(FlashContext);
+  const { user: currentUser } = useContext(UserContext);
+  const { flash } = useContext(FlashContext);
 
   const addComment = (e) => {
     e.preventDefault();
 
     if (!currentUser.uid) {
-      flash({show: true, msg: "Please Sign In First!", success: false});
+      flash({ show: true, msg: "Please Sign In First!", success: false });
       return;
     }
 
@@ -38,23 +39,23 @@ export default function NewComment({commentsColRef, postId}) {
   };
 
   return (
-      <form
-          className="grid gap-2.5 bg-primary border border-[#ccc] p-3 rounded-lg shadow-lg
+    <form
+      className="grid gap-2.5 bg-primary border border-[#ccc] p-3 rounded-lg shadow-lg
     dark:bg-darkText dark:border-primary dark:text-primary"
-      >
+    >
       <textarea
-          placeholder="New Post"
-          value={commentText}
-          className="w-full bg-primary h-full p-2 lg:p-4 border-2 border-secondary focus:outline-none
+        placeholder="Write a comment"
+        value={commentText}
+        className="w-full bg-primary h-full p-2 lg:p-4 border-2 border-secondary focus:outline-none
                         dark:bg-darkText dark:border-[#ccc] dark:text-primary rounded-lg"
-          onChange={(e) => setCommentText(e.target.value)}
+        onChange={(e) => setCommentText(e.target.value)}
       />
-        <button
-            className="border-2 border-secondary py-2 px-6 rounded-full justify-self-end transition-colors hover:bg-secondary"
-            onClick={addComment}
-        >
-          Submit
-        </button>
-      </form>
+      <button
+        className="border-2 border-secondary py-2 px-6 rounded-full justify-self-end transition-colors hover:bg-secondary"
+        onClick={addComment}
+      >
+        Submit
+      </button>
+    </form>
   );
 }
