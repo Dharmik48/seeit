6fd7ebd72ab277459cd0f073b6e9d59c953e5cfe
Dharmik_403 <hingudharmik4@gmail.com>
2022-08-27 20:22:05 +0530

    feat: sets up users route and render user component

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import FlashContext from "./contexts/FlashContext.jsx";
 import UserContext from "./contexts/UserContext.jsx";
 // COMPONENTS
-import NewPost from "./components/posts/NewPost.jsx";
 import Posts from "./components/posts/Posts.jsx";
+import User from "./components/User.jsx";
 import FlashMsg from "./components/FlashMsg";
 import Header from "./components/Header";
 import ThemeToggle from "./components/ThemeToggle.jsx";
@@ -13,32 +13,32 @@ import { useEffect } from "react";
 import { auth } from "./firebase/firebase.js";
 
 function App() {
-    const { user, setUser } = useContext(UserContext);
-    const { flashData } = useContext(FlashContext);
-    useEffect(() => {
-        auth.onAuthStateChanged((user) => {
-            if (user) {
-                const { uid, photoURL, email, displayName } = user;
-                setUser({ uid, photoURL, email, displayName });
-            } else {
-                console.log("not logged in");
-            }
-        });
-    }, []);
-    return (
-        <main className="w-full min-h-screen py-5 bg-secondary px-5 font-primary dark:bg-darkText">
-            {flashData?.show && <FlashMsg />}
-            <Header />
-            <section className="max-w-xl mx-auto grid relative">
-                <ThemeToggle />
-                {user && user.email && <NewPost />}
-                <Routes>
-                    <Route exact path="/" element={<Posts />} />
-                    <Route path="/posts/:postId" element={<PostDetail />} />
-                </Routes>
-            </section>
-        </main>
-    );
+  const { setUser } = useContext(UserContext);
+  const { flashData } = useContext(FlashContext);
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        const { uid, photoURL, email, displayName } = user;
+        setUser({ uid, photoURL, email, displayName });
+      } else {
+        console.log("not logged in");
+      }
+    });
+  }, []);
+  return (
+    <main className="w-full min-h-screen py-5 bg-secondary px-5 font-primary dark:bg-darkText">
+      {flashData?.show && <FlashMsg />}
+      <Header />
+      <section className="max-w-xl mx-auto grid relative">
+        <ThemeToggle />
+        <Routes>
+          <Route exact path="/" element={<Posts />} />
+          <Route path="/posts/:postId" element={<PostDetail />} />
+          <Route path={"/users/:userId"} element={<User />} />
+        </Routes>
+      </section>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/User.jsx
@@ -0,0 +1,5 @@
+const User = () => {
+  return <h1>USER!!</h1>;
+};
+
+export default User;
