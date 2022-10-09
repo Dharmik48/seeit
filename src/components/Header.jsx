2bf00758d5a97b77a4e48b4e48020d03ea470851
Hemant <hrsmart2150@gmail.com>
2022-10-09 19:22:41 +0530

    Improved the UI of profile page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import Auth from "./Auth";
+import UserContext from "../contexts/UserContext";
+import ThemeToggle from "./ThemeToggle";
 
 const Header = () => {
+  const user = useContext(UserContext);
   return (
-    <header className="max-w-3xl flex justify-between mx-auto border-b border-darkText pb-3 lg:pb-5 dark:border-gray">
+    <header className="max-w-3xl flex justify-between items-center mx-auto border-b border-darkText pb-3 lg:pb-5 dark:border-gray">
       <Link to={"/"}>
         <h1 className="text-2xl dark:text-primary">👀 SeeIt</h1>
       </Link>
+      <ThemeToggle />
       <Auth />
     </header>
   );
